32992831784f50d231c42ff4e924796d89ce986f
shlomil <shlomil@reali.com>
2020-08-03 17:15:32 +0300

    update mongo string

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/meanauth',
     // for heroku
-     database: 'mongodb+srv://meanauth:meanauth@meanauthapp.ldoph.mongodb.net/meanauth?retryWrites=true&w=majority',
+     database: 'mongodb+srv://meanauth:meanauth@meanauthapp.ldoph.mongodb.net/meanauthapp?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
